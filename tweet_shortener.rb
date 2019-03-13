06a148080fe0a45b2b2d39f3a4b4ddd3fbd8d3c9
Daniel Hallada <hallada.daniel@gmail.com>
2019-03-13 14:25:02 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,33 @@
 # Write your code here.
+def dictionary
+  list = {
+    "hello" => "hi",
+    "to" => "2",
+    "too" => "2",
+    "two" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+
+end
+
+def word_substituter(tweet)
+  new = []
+  new2 = dictionary.keys
+  new.each do |word|
+    if new2.include?(word)
+      new.push(dictionary[word])
+    else
+      new.push(word)
+    end
+  end
+
+end
+
+def bulk_tweet_shortener(tweet)
+
+end
